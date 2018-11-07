fb99b4da9420fa1e332eeb80dbb8d56222495478
Chuba Oraka <17247707+ChubaOraka@users.noreply.github.com>
2018-11-07 21:22:08 +0000

    Second Commit

---
+++ PictureBotC1-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=picturebotc1-Web-Deploy.pubxml -p:Password=E7Me25ScbvA2FLdli1uc82rwANeWoFSLEXlRi1blGSAgJtpfdsKyuLAj1mJh
+
